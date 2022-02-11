eb101482ed68965a3b3d142f0b303c84789f4c77
GlitchGamer1459 <glitchgamer1459@gmail.com>
2022-02-10 18:46:30 -0800

    aaa

--- Main.cpp
+++ Main.cpp
@@ -1,33 +1,6 @@
 #include <iostream>
-#include <chrono>
-#include <thread>
-#include <functional>
 #include "engine.h"
 using namespace std;
-
-class Target {
-public:
-	double x, y;
-	double hitboxRadius = .5;
-
-	Target(double xin, double yin) {
-		x = xin;
-		y = yin;
-	}
-};
-
-class Rock {
-public:
-	double x, y, velocity, direction;
-	double hitboxRadius = .1;
-
-	Rock(double xin, double yin, double dirin, double velin) {
-		x = xin;
-		y = yin;
-		velocity = velin;
-		direction = dirin;
-	}
-};
 
 int tickCount = 0;
 
@@ -36,7 +9,6 @@ void start() {
 }
 
 void update() {
-	
 
 	if (tickCount == 300) {
 		cout << "game tick: " << tickCount << endl;
